f66c3914f1264d1d0ee9a0d5ce9d1fcfda021988
Michelle <michellesantiagobeadle@hotmail.com>
2019-10-30 10:30:27 -0300

    Grid inicial do jogo pronta e ajustável

--- RogueLoros Game/Assets/Scripts/Grid/GridManager.cs
+++ RogueLoros Game/Assets/Scripts/Grid/GridManager.cs
@@ -4,32 +4,61 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    public int maxLinesPerGrid = 5;
     public GameObject linePrefab;
+    public GameObject nodePrefab;   // usado meramente para calcular o numero de linhas na tela
+    public GameObject initialNode;
 
-    private List<List<GameObject>> grid = new List<List<GameObject>>();
+    public float offSetBetweenLines = 0.5f;
 
-    /*GridManager() {
-        createInitialGrid();
-        Debug.Log("Criei Grid");
-    }*/
+    private List<List<GameObject>> nodeList = new List<List<GameObject>>();
+    private List<GameObject> lineList = new List<GameObject>();
+
+    private int maxLinesPerGrid = 4;
+    private float nodeSize;
+
+    private int lineCounter = 0;
 
     private void Start() {
+        calculateMaxNumOfLines();
         createInitialGrid();
     }
 
-    // -------- Funções relativas a criar a grid -----------
+    // -------- Funções relativas a criar a grid inicial -----------
 
     private void createInitialGrid() {
 
         for (int i=0; i<maxLinesPerGrid; i++) {
 
-            GameObject line = Instantiate(linePrefab);
-            line.transform.SetParent(this.transform);
+            GameObject line = Instantiate(linePrefab, calculatePostitionInWorld(i), linePrefab.transform.rotation, this.transform);
+            line.GetComponent<LineInstance>().ID = lineCounter;
+            this.lineList.Add(line);
 
             List<GameObject> nodeList = line.GetComponent<LineInstance>().getNodeList();
-            grid.Add(nodeList);
+            this.nodeList.Add(nodeList);
+
+            lineCounter++;
         }
+
+    }
+
+    private void calculateMaxNumOfLines() {
+
+        int height = Screen.height;
+
+        // Utiliza o tamanho do node para calcular quantas linhas caberão na tela - fake
+        nodeSize = nodePrefab.GetComponent<Renderer>().bounds.size.y;
+
+        // substituir maxLinesPerGrid = numLinesVertical quando for essa funcao estiver direita
+        int numLinesVertical = (int) (height / (nodeSize + offSetBetweenLines));
+        //Debug.Log(numLinesVertical);
+    }
+
+    private Vector3 calculatePostitionInWorld(int i) {
+
+        Vector3 linePos = new Vector3(0, initialNode.transform.position.y + (nodeSize + offSetBetweenLines) + (nodeSize + offSetBetweenLines) * i, 0);
+        //Vector3 lineWorldPos = Camera.main.ScreenToWorldPoint(linePos);
+
+        return linePos;
 
     }
 
--- RogueLoros Game/Assets/Scripts/Grid/LineInstance.cs
+++ RogueLoros Game/Assets/Scripts/Grid/LineInstance.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class LineInstance : MonoBehaviour
 {
-    public int maxNodesInLine = 6;
+    [HideInInspector] public int ID = 0;
+
+    public int maxNodesInLine = 7;
     // fazer um array para sortear os nodes
     public GameObject nodePrefab;
 
+    public float offSetBetweenNodes = 1.5f;
+
     private List<GameObject> nodeList = new List<GameObject>();
+    private float nodeSize;
 
     private void Start() {
         createNodeList();
@@ -26,8 +31,8 @@ public class LineInstance : MonoBehaviour
 
         for (int i=0; i<maxNodesInLine; i++) {
 
-            GameObject node = Instantiate(nodePrefab);      // cria uma copia do prefab
-            node.transform.SetParent(this.transform);       // Coloca o node como filho da linha
+            GameObject node = Instantiate(nodePrefab, calculatePostitionInWorld(i), nodePrefab.transform.rotation, this.transform);      // cria uma copia do prefab
+            //node.transform.SetParent(this.transform);       // Coloca o node como filho da linha
 
             // seta as infos do node
             //node = node.GetComponent<NodeInstance>().createNode();
@@ -35,6 +40,70 @@ public class LineInstance : MonoBehaviour
             // adiciona o node à lista de nodes da linha
             nodeList.Add(node);
         }
+    }
+
+    private void calculateMaxNumOfLines() {
+
+        int width = Screen.width;
+
+        // Utiliza o tamanho do node para calcular quantas linhas caberão na tela - fake
+        nodeSize = nodePrefab.GetComponent<Renderer>().bounds.size.x;
+
+        // substituir maxLinesPerGrid = numLinesVertical quando for essa funcao estiver direita
+        int numLinesHorizontal = (int) (width / (nodeSize + offSetBetweenNodes));
+        //Debug.Log(numLinesHorizontal);
+    }
+
+    private Vector3 calculatePostitionInWorld(int i) {
+
+        Vector3 linePos;
+
+        // Verifica se a linha é par
+        if (ID % 2 == 0) {
+
+            if (i % 2 == 0) {   // dispôe os blocos pares a direita e os impares à esquerda
+
+                // Se o index é 2 ele passa a ser 1 e fica ao lado do item 0
+                int index = 0;
+                if (i > 0)
+                    index = i - i/2;
+
+                linePos = new Vector3((nodeSize + offSetBetweenNodes) / 2 + (nodeSize + offSetBetweenNodes) * index, 0, 0);
+            }else {
+
+                // Se o index é 3 ele passa a ser 2 e fica ao lado do item 0
+                int index = 1;
+                if (i > 1)
+                    index = i - (i/2);
+
+                linePos = new Vector3((nodeSize + offSetBetweenNodes) / 2 + (nodeSize + offSetBetweenNodes) * -index, 0, 0);
+            }
+
+        } else {
+
+            if (i % 2 == 0) {   // dispôe os blocos pares a direita e os impares à esquerda
+
+                int index = 0;
+                if (i > 0)
+                    index = i - i/2;
+
+                linePos = new Vector3((nodeSize + offSetBetweenNodes) * index, 0, 0);
+            } else {
+
+                int index = 1;
+                if (i > 1)
+                    index = i - (i / 2);
+
+                linePos = new Vector3((nodeSize + offSetBetweenNodes) * -index, 0, 0);
+            }
+        }
+
+        //Vector3 lineWorldPos = Camera.main.ScreenToWorldPoint(linePos);
+
+        linePos = this.transform.TransformPoint(linePos);
+
+        return linePos;
+
     }
 
 }
