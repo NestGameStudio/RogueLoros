f71b507b166d5f898d975fc073448f3b8390e87b
Michelle <michellesantiagobeadle@hotmail.com>
2019-11-05 20:15:49 -0300

    vai

---
+++ RogueLoros Game/Assets/Scripts/NodeActions/ActionEnemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionEnemy : NodeAction
+{
+    public override void DoAction() {
+        base.DoAction();
+
+        Debug.Log("Perde vida");
+    }
+
+    public override void EndAction() {
+        base.EndAction();
+    }
+}
---
+++ RogueLoros Game/Assets/Scripts/NodeActions/ActionGainLife.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionGainLife : NodeAction {
+
+    public override void DoAction() {
+        base.DoAction();
+        Debug.Log("GANHA VIDA");
+    }
+
+    public override void EndAction() {
+        base.EndAction();
+    }
+}
