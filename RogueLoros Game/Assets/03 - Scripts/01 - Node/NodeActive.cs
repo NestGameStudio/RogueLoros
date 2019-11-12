b0b8a6795eecd05173a1ca0ad84fee64da62bfcb
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2019-11-12 14:10:40 -0300

    some cleaning

--- RogueLoros Game/Assets/03 - Scripts/01 - Node/NodeActive.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NodeActive : MonoBehaviour
-{
-    // Unica razao da existencia desse script é ser uma forma de avaliar se o node é util ou nao;
-    // Talvez usar uma bool publica no Node Instance?
-}
