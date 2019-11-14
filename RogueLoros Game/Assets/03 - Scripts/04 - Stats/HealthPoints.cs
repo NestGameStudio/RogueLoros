8cd450e8618f6c0258494ddfc10522ebbdd520ae
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2019-11-14 19:02:35 -0300

    inicio stats

---
+++ RogueLoros Game/Assets/03 - Scripts/04 - Stats/HealthPoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPoints: Stats {
+
+    [Space(20)]
+
+    public int Level = 1;
+    
+
+    public int MaxValue = 3;
+
+
+    private int currentValue = 3;
+
+
+    
+}
---
+++ RogueLoros Game/Assets/03 - Scripts/04 - Stats/Stats.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Stats: MonoBehaviour {
+
+    [Header("Experience Manager")]
+    [Space(5)]
+
+    [NamedListAttribute("Level Up XP")]
+    public List<int> LevelCap;
+
+
+}
+
+// Permite que o valor da lista de nivel seja atribuido
+public class NamedListAttribute : PropertyAttribute {
+    public readonly string ListElementName;
+    public NamedListAttribute(string ListElementName) { this.ListElementName = ListElementName; }
+}
+
+// A lista de forma customizável
+[CustomPropertyDrawer(typeof(NamedListAttribute))]
+public class NamedArrayDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label) {
+        try {
+            //int pos = int.Parse(property.propertyPath.Split('[', ']')[1]);
+            EditorGUI.PropertyField(rect, property, new GUIContent(((NamedListAttribute)attribute).ListElementName));
+        } catch {
+            EditorGUI.PropertyField(rect, property, label);
+        }
+    }
+}
