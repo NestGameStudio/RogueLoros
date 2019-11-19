fc3df30050609ea4a317dadee933f3224ac8238d
lucas3311 <lucaslimareis3311@gmail.com>
2019-11-18 23:14:48 -0300

    Animations offset

---
+++ RogueLoros Game/Assets/03 - Scripts/06 - Art/ChangeAnimStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator anim = gameObject.GetComponentInParent<Animator>();
+
+        float randomIdleStart = Random.Range(0, anim.GetCurrentAnimatorStateInfo(0).length); //Set a random part of the animation to start from
+        anim.Play("Tile", 0, randomIdleStart);
+
+    }
+
+}
