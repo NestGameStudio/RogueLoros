6094a4d4e2eab9fb499012f7d6508dc50a8c7c64
Michelle <michellesantiagobeadle@hotmail.com>
2019-12-27 17:40:03 -0300

    comentarios

--- RogueLoros Game/Assets/03 - Scripts/01 - Node/NodeActions/ActionChest.cs
+++ RogueLoros Game/Assets/03 - Scripts/01 - Node/NodeActions/ActionChest.cs
@@ -18,6 +18,10 @@ public class ActionChest : NodeAction {
             PlayerInstance.Instance.IncreaseHealth(currentChest.HealValue);
             ExperienceManager.Instance.IncreaseXPPoints(currentChest.XP);
 
+            Debug.Log("Dinheiro: " + currentChest.Coin);
+            Debug.Log("Vida: " + currentChest.HealValue);
+            Debug.Log("XP: " + currentChest.XP);
+
             // randomiza quais dos feiticos possiveis podem sair
             if (currentChest.Feiticos.Length > 0) {
 
@@ -25,6 +29,8 @@ public class ActionChest : NodeAction {
 
                 // cria o feitico
                 GameObject Spell = SpellManager.Instance.CreateSpell(currentChest.Feiticos[typeIndex]);
+
+                Debug.Log("Feitiços: " + currentChest.Feiticos[typeIndex]);
 
                 // Verifica quantos espacos vazios ainda tem na HUD de feiticos
                 // Adiciona o feitico a lista de feiticos
